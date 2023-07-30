20e06ee1205dbab445391c32fa71555b3329b7b2
Artem Andriushchenko <link.by.artem@gmail.com>
2023-07-30 18:36:41 +0300

    add Question 18 19 20

---
+++ src/EntryTest/Question_18.java
@@ -0,0 +1,13 @@
+package EntryTest;
+
+public class Question_18 {
+    /* What correct syntax of html hyperlink?
+
+
+       1) <a> epam.com </a>
+       2) <hyperlink> epam.com </hyperlink>
+       3) <a href="epam.com"> EPAM </a> ✔
+       4) None of the above
+                 ️
+     */
+}
---
+++ src/EntryTest/Question_19.java
@@ -0,0 +1,20 @@
+package EntryTest;
+
+public class Question_19 {
+    /* Does Java have operator overloading?
+
+
+       1) Yes, but the user cannot define their own overload
+       2) Yes, the user can define their own overload
+       3) No, it is not present at all ✔
+
+
+       Detailed response:
+
+       Java не поддерживает перегрузку операторов, как это делают некоторые другие языки программирования, например C++. \
+       Перегрузка оператора означает возможность определить несколько вариантов поведения оператора на основе типов данных его операндов.
+       В Java операторы имеют фиксированные значения в зависимости от типов данных, к которым они применяются,
+       и эти значения не могут быть изменены или расширены пользователем.
+                 ️
+     */
+}
